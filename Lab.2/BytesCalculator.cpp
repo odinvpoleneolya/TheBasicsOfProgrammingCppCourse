254e48552f4b1e69ac09621c2050c96b02b10304
Olha Boiko <olchykboyko@icloud.com>
2024-10-08 10:18:30 +0300

    Lab.2

---
+++ Lab.2/BytesCalculator.cpp
@@ -0,0 +1,31 @@
+﻿// BytesCalculator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "bool:\t\t" << sizeof(bool) << " bytes" << std::endl;
+    std::cout << "char:\t\t" << sizeof(char) << " bytes" << std::endl;
+    std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << std::endl;
+    std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << std::endl;
+    std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << std::endl;
+    std::cout << "short:\t\t" << sizeof(short) << " bytes" << std::endl;
+    std::cout << "int:\t\t" << sizeof(int) << " bytes" << std::endl;
+    std::cout << "long:\t\t" << sizeof(long) << " bytes" << std::endl;
+    std::cout << "long long:\t" << sizeof(long long) << " bytes" << std::endl;
+    std::cout << "float:\t\t" << sizeof(float) << " bytes" << std::endl;
+    std::cout << "double:\t\t" << sizeof(double) << " bytes" << std::endl;
+    std::cout << "long double:\t" << sizeof(long long) << " bytes" << std::endl;
+    return 0;
+}
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
